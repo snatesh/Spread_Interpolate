5d3dd922488515c749241a35711d0edb1762640f
snatesh <sachin.natesh@gmail.com>
2020-04-29 06:57:05 -0400

    initial build system config

---
+++ include/io.h
@@ -0,0 +1,7 @@
+#ifndef IO_H
+#define IO_H
+
+void write_to_file(const double* F, const unsigned int N, const char* fname);
+void write_coords(const unsigned int N, const double h, const char* fname);
+
+#endif
---
+++ include/spread_interp.h
@@ -0,0 +1,36 @@
+#ifndef _SPREAD_INTERP_H
+#define _SPREAD_INTERP_H
+
+#pragma omp declare simd
+inline double const deltaf(const double x, const double y, const double z);
+
+
+// flattened index into 3D array
+#pragma omp declare simd
+inline unsigned int const at(unsigned int i, unsigned int j,unsigned int k,\
+                             const unsigned int Nx, const unsigned int Ny);
+
+
+// flatted index into 3D array, corrected for periodic boundaries
+#pragma omp declare simd
+inline unsigned int const pbat(int i, int j, int k, const int Nx, const int Ny);
+
+
+// flatted index into 3D array, corrected for periodic boundaries
+#pragma omp declare simd
+inline unsigned int const pbat(int i, int j, int k, const int Nx, const int Ny, const int Nz);
+
+
+
+// gather data from src at inds into trg
+inline void gather(unsigned int N, double* trg, double const* src, const unsigned int* inds);
+
+// scatter data from trg at inds into src
+
+inline void scatter(unsigned int N, double const* trg, double* src, const unsigned int* inds);
+
+// spreading (mode=true) and interpolation (mode=false)
+void spread_interp(double* xp, double* fl, double* Fe, int* firstn, 
+                   int* nextn, unsigned int* number, const unsigned short w, 
+                   const double h, const unsigned short N, const bool mode);
+#endif
