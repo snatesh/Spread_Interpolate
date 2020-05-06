1ca696cb96718c8f120714fbbd0bb357bf3ba23c
snatesh <sachin.natesh@gmail.com>
2020-05-06 16:24:23 -0400

    add fftw to build sys

--- src/spread_interp.cpp
+++ src/spread_interp.cpp
@@ -3,137 +3,6 @@
 #ifdef DEBUG
 #include<iostream>
 #endif
-void spread_interp(double* xp, double* fl, double* Fe, int* firstn, 
-                   int* nextn, unsigned int* number, const unsigned short w, 
-                   const double h, const unsigned short N, const bool mode)
-{
-  const double weight = (mode ? 1 : h * h * h);
-  const unsigned short w2 = w * w; const unsigned int N2 = N * N; 
-
-  // loop over w^2 groups of columns
-  for (unsigned int izero = 0; izero < w; ++izero)
-  {
-    for (unsigned int jzero = 0; jzero < w; ++jzero)
-    {
-      // parallelize over the N^2/w^2 columns in a group
-      #pragma omp parallel for
-      for (unsigned int ijcount = 0; ijcount < N2/w2; ++ijcount)
-      {
-        // column indices
-        unsigned int jj = jzero + w * (ijcount / (N / w));
-        unsigned int ii = izero + w * (ijcount % (N / w));
-        
-        // find first particle in column(ii,jj) and continue if it's there
-        int l = firstn[jj + ii * N];
-        if (l >= 0 )
-        {
-          // global indices of w x w x N subarray influenced column(i,j)
-          alignas(MEM_ALIGN) unsigned int indc3D[w2 * N];
-          for (int i = 0; i < w; ++i) 
-          {
-            int i3D = ii + i - w/2 + 1;
-            for (int j = 0; j < w; ++j)
-            {
-              int j3D = jj + j - w/2 + 1;
-              for (int k3D = 0; k3D < N; ++k3D)
-              {
-                indc3D[at(i,j,k3D,w,w)] = pbat(i3D,j3D,k3D,N,N);
-              }
-            }
-          }
-          
-          // gather eulerian foces for one column into contig mem Fec
-          alignas(MEM_ALIGN) double Fec[w2 * N * 3];  
-          gather(w2 * N, Fec, Fe, indc3D);
-          
-          // number of pts in this column, particle indices
-          unsigned int npts = number[jj + ii * N], indx[npts];
-          for (unsigned int ipt = 0; ipt < npts; ++ipt) {indx[ipt] = l; l = nextn[l];}
-          
-          // gather lagrangian pts and forces into xpc and flc
-          alignas(MEM_ALIGN) double xpc[npts * 3]; 
-          alignas(MEM_ALIGN) double flc[npts * 3]; 
-          gather(npts, xpc, xp, indx);
-          gather(npts, flc, fl, indx);
-          // get the kernel w x w x w kernel weights for each particle in col 
-          alignas(MEM_ALIGN) double delta[w2 * w * npts];
-          for (int j = 0; j < w; ++j)
-          {
-            // unwrapped y coordinates
-            double fj = ((double) jj + j - w/2 + 1) * h;
-            for (int i = 0; i < w; ++i)
-            {
-              // unwrapped x coordinates
-              double fi = ((double) ii + i - w/2 + 1) * h;
-              for (int k = 0; k < w; ++k)
-              {
-                unsigned int m = at(i,j,k,w,w);
-                #pragma omp simd // vectorization over particles in col
-                for (unsigned int ipt = 0; ipt < npts; ++ipt) 
-                {
-                  // unwrapped z coordinates
-                  double fk = (double) (((int)xpc[2 + 3 * ipt] / h) + k - w/2 + 1) * h;
-                  // kernel weights 
-                  delta[ipt + m * npts] = deltaf(xpc[3 * ipt] - fi, \
-                                                 xpc[1 + 3 * ipt] - fj, \
-                                                 xpc[2 + 3 * ipt] - fk) * weight;  
-
-                }
-              }
-            }
-          }
-  
-          if (mode)
-          {         
-            // update Eulerian density
-            int offset1 = w2 * N;
-            for (unsigned int ipt = 0; ipt < npts; ++ipt)
-            {
-              int m0 = ((int) xpc[2 + 3 * ipt] / h - w/2 + 1);
-              int offset0 = w2 * m0;
-              #pragma omp simd // vectorize over eulerian pts
-              for (int i = 0; i < w2 * w; ++i)
-              {
-                int k = i / w2 + m0, ic = i + offset0;
-                if (k < 0) ic = i + offset0 + offset1;
-                if (k >= N) ic = i + offset0 - offset1;
-                Fec[3 * ic] += delta[ipt + i * npts] * flc[3 * ipt]; 
-                Fec[1 + 3 * ic] += delta[ipt + i * npts] * flc[1 + 3 * ipt]; 
-                Fec[2 + 3 * ic] += delta[ipt + i * npts] * flc[2 + 3 * ipt]; 
-              }
-            }
-            // scatter back to global eulerian grid
-            scatter(w2 * N, Fec, Fe, indc3D);
-          } 
-          else
-          {
-            // interpolate lagrangian density
-            int offset1 = w2 * N;
-            for (int i = 0; i < w2 * w; ++i)
-            {
-              #pragma omp simd // vectorize over lagrangian pts 
-              for (unsigned int ipt = 0; ipt < npts; ++ipt)
-              {
-                int m0 = ((int) xpc[2 + 3 * ipt] / h - w/2 + 1);
-                int offset0 = w2 * m0;
-                int k = i / w2 + m0, ic = i + offset0;
-                if (k < 0) ic = i + offset0 + offset1;
-                if (k >= N) ic = i + offset0 - offset1;
-                flc[3 * ipt] += Fec[3 * ic] * delta[ipt + i * npts]; 
-                flc[1 + 3 * ipt] += Fec[1 + 3 * ic] * delta[ipt + i * npts]; 
-                flc[2 + 3 * ipt] += Fec[2 + 3 * ic] * delta[ipt + i * npts]; 
-              }
-            }   
-            // scatter back to global lagrangian grid
-            scatter(npts, flc, fl, indx);
-          }
-        } 
-      // go to next column in group
-      } 
-    // go to next group
-    }
-  }
-}    
 
 void spread_interp_pbc(double* xp, double* fl, double* Fe, double* Fe_wrap, int* firstn, 
                    int* nextn, unsigned int* number, const unsigned short w, 
@@ -158,7 +27,6 @@ void spread_interp_pbc(double* xp, double* fl, double* Fe, double* Fe_wrap, int*
         unsigned int jj = jzero + w * (ijcount / (N / w));
         unsigned int ii = izero + w * (ijcount % (N / w));
 
-        
         // find first particle in column(ii,jj) and continue if it's there
         int l = firstn[jj + ii * N];
         if (l >= 0 )
@@ -173,7 +41,7 @@ void spread_interp_pbc(double* xp, double* fl, double* Fe, double* Fe_wrap, int*
               for (int i = 0; i < w; ++i) 
               {
                 int i3D = ii + i - w/2 + 1;
-                  indc3D[at(i,j,k3D,w,w)] = at(i3D,j3D,k3D,N,N);
+                indc3D[at(i,j,k3D,w,w)] = at(i3D,j3D,k3D,N,N);
               }
             }
           }
@@ -188,7 +56,6 @@ void spread_interp_pbc(double* xp, double* fl, double* Fe, double* Fe_wrap, int*
             gather(w2 * N, Fec, Gec, Hec, Fe, indc3D);
           #endif 
 
-          
           // number of pts in this column, particle indices
           unsigned int npts = number[jj + ii * N];
           alignas(MEM_ALIGN) unsigned int indx[npts];
@@ -332,9 +199,141 @@ void spread_interp_pbc(double* xp, double* fl, double* Fe, double* Fe_wrap, int*
   if (mode) fold_pbc(Fe, Fe_wrap, w, N);
 }
 
-#ifdef SEPXYZ
+void spread_interp(double* xp, double* fl, double* Fe, int* firstn, 
+                   int* nextn, unsigned int* number, const unsigned short w, 
+                   const double h, const unsigned short N, const bool mode)
+{
+  const double weight = (mode ? 1 : h * h * h);
+  const unsigned short w2 = w * w; const unsigned int N2 = N * N; 
 
-#else
+  // loop over w^2 groups of columns
+  for (unsigned int izero = 0; izero < w; ++izero)
+  {
+    for (unsigned int jzero = 0; jzero < w; ++jzero)
+    {
+      // parallelize over the N^2/w^2 columns in a group
+      #pragma omp parallel for
+      for (unsigned int ijcount = 0; ijcount < N2/w2; ++ijcount)
+      {
+        // column indices
+        unsigned int jj = jzero + w * (ijcount / (N / w));
+        unsigned int ii = izero + w * (ijcount % (N / w));
+        
+        // find first particle in column(ii,jj) and continue if it's there
+        int l = firstn[jj + ii * N];
+        if (l >= 0 )
+        {
+          // global indices of w x w x N subarray influenced column(i,j)
+          alignas(MEM_ALIGN) unsigned int indc3D[w2 * N];
+          for (int i = 0; i < w; ++i) 
+          {
+            int i3D = ii + i - w/2 + 1;
+            for (int j = 0; j < w; ++j)
+            {
+              int j3D = jj + j - w/2 + 1;
+              for (int k3D = 0; k3D < N; ++k3D)
+              {
+                indc3D[at(i,j,k3D,w,w)] = pbat(i3D,j3D,k3D,N,N);
+              }
+            }
+          }
+          
+          // gather eulerian foces for one column into contig mem Fec
+          alignas(MEM_ALIGN) double Fec[w2 * N * 3];  
+          gather(w2 * N, Fec, Fe, indc3D);
+          
+          // number of pts in this column, particle indices
+          unsigned int npts = number[jj + ii * N], indx[npts];
+          for (unsigned int ipt = 0; ipt < npts; ++ipt) {indx[ipt] = l; l = nextn[l];}
+          
+          // gather lagrangian pts and forces into xpc and flc
+          alignas(MEM_ALIGN) double xpc[npts * 3]; 
+          alignas(MEM_ALIGN) double flc[npts * 3]; 
+          gather(npts, xpc, xp, indx);
+          gather(npts, flc, fl, indx);
+          // get the kernel w x w x w kernel weights for each particle in col 
+          alignas(MEM_ALIGN) double delta[w2 * w * npts];
+          for (int j = 0; j < w; ++j)
+          {
+            // unwrapped y coordinates
+            double fj = ((double) jj + j - w/2 + 1) * h;
+            for (int i = 0; i < w; ++i)
+            {
+              // unwrapped x coordinates
+              double fi = ((double) ii + i - w/2 + 1) * h;
+              for (int k = 0; k < w; ++k)
+              {
+                unsigned int m = at(i,j,k,w,w);
+                #pragma omp simd // vectorization over particles in col
+                for (unsigned int ipt = 0; ipt < npts; ++ipt) 
+                {
+                  // unwrapped z coordinates
+                  double fk = (double) (((int)xpc[2 + 3 * ipt] / h) + k - w/2 + 1) * h;
+                  // kernel weights 
+                  delta[ipt + m * npts] = deltaf(xpc[3 * ipt] - fi, \
+                                                 xpc[1 + 3 * ipt] - fj, \
+                                                 xpc[2 + 3 * ipt] - fk) * weight;  
+
+                }
+              }
+            }
+          }
+  
+          if (mode)
+          {         
+            // update Eulerian density
+            int offset1 = w2 * N;
+            for (unsigned int ipt = 0; ipt < npts; ++ipt)
+            {
+              int m0 = ((int) xpc[2 + 3 * ipt] / h - w/2 + 1);
+              int offset0 = w2 * m0;
+              #pragma omp simd // vectorize over eulerian pts
+              for (int i = 0; i < w2 * w; ++i)
+              {
+                int k = i / w2 + m0, ic = i + offset0;
+                if (k < 0) ic = i + offset0 + offset1;
+                if (k >= N) ic = i + offset0 - offset1;
+                Fec[3 * ic] += delta[ipt + i * npts] * flc[3 * ipt]; 
+                Fec[1 + 3 * ic] += delta[ipt + i * npts] * flc[1 + 3 * ipt]; 
+                Fec[2 + 3 * ic] += delta[ipt + i * npts] * flc[2 + 3 * ipt]; 
+              }
+            }
+            // scatter back to global eulerian grid
+            scatter(w2 * N, Fec, Fe, indc3D);
+          } 
+          else
+          {
+            // interpolate lagrangian density
+            int offset1 = w2 * N;
+            for (int i = 0; i < w2 * w; ++i)
+            {
+              #pragma omp simd // vectorize over lagrangian pts 
+              for (unsigned int ipt = 0; ipt < npts; ++ipt)
+              {
+                int m0 = ((int) xpc[2 + 3 * ipt] / h - w/2 + 1);
+                int offset0 = w2 * m0;
+                int k = i / w2 + m0, ic = i + offset0;
+                if (k < 0) ic = i + offset0 + offset1;
+                if (k >= N) ic = i + offset0 - offset1;
+                flc[3 * ipt] += Fec[3 * ic] * delta[ipt + i * npts]; 
+                flc[1 + 3 * ipt] += Fec[1 + 3 * ic] * delta[ipt + i * npts]; 
+                flc[2 + 3 * ipt] += Fec[2 + 3 * ic] * delta[ipt + i * npts]; 
+              }
+            }   
+            // scatter back to global lagrangian grid
+            scatter(npts, flc, fl, indx);
+          }
+        } 
+      // go to next column in group
+      } 
+    // go to next group
+    }
+  }
+}    
+
+
+
+#ifndef SEPXYZ
 void delta_col(double* delta, const double* xpc, const double fi, 
                    const double fj, const double weight, const int m, 
                    const int k, const int npts, const int w, const int h)
@@ -378,7 +377,4 @@ void interp_col(const double* Fec, const double* delta, double* flc,
   }
   flc[3 * ipt] += flsum; flc[1 + 3 * ipt] += glsum; flc[2 + 3 * ipt] += hlsum;
 }
-
 #endif
-
-
